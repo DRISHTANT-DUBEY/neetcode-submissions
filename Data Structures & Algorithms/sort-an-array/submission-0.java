5f17d8ba40995ea13bc7431a3f58cee6fc3425f4
Drishtant Dubey <70314749+DRISHTANT-DUBEY@users.noreply.github.com>
2026-08-09 18:31:56 +0530

    Add: sort-an-array - submission-0

---
+++ Data Structures & Algorithms/sort-an-array/submission-0.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int[] sortArray(int[] nums) {
+        //Min Max of nums 
+        int min = nums[0], max = nums[0];
+        //counting sort 
+        //create a hash map and the based on keys and its freqency create the sorted array 
+        Map<Integer, Integer> mp = new HashMap<>();
+        for(int i=0; i<nums.length; i++ ){
+            mp.put(nums[i], mp.getOrDefault(nums[i], 0) + 1);
+            
+            if (nums[i] < min){
+                min = nums[i];
+            }
+            
+            if (nums[i] > max){
+                max = nums[i];
+            }
+        }
+
+        int index = 0;
+        for (int i = min; i <= max; i++){
+            while (mp.getOrDefault(i, 0)>0){
+              //copy the same element from the map
+              nums[index]= i;
+              index++;
+              //decrement the frequncy in the map
+              mp.put(i, mp.get(i)-1);
+            }
+        }
+
+        return nums;
+
+        
+    }
+}
